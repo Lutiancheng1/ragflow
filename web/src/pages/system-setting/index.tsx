765adf502d82730f55d57ee3feb2bf16f9fd0dcc
Lutiancheng1 <2372542263@qq.com>
2024-12-21 16:34:03 +0800

    fix login background、 autologin account

--- web/src/pages/system-setting/index.tsx
+++ web/src/pages/system-setting/index.tsx
@@ -1,11 +1,7 @@
 export default function SystemSetting() {
   return (
     <div className="w-full h-full">
-      <iframe
-        src="https://demo.gotoai.world/admin/auto-login?token=YWRtaW5AZXhhbXBsZS5jb206YWRtaW4xMjM="
-        frameBorder="0"
-        className="w-full h-full"
-      ></iframe>
+      <iframe src="https://demo.gotoai.world/admin/auto-login?token=Z290b2FpQGdvdG9haS53b3JsZDpIZWxsbzEyMw==" frameBorder="0" className="w-full h-full"></iframe>
     </div>
-  );
+  )
 }
